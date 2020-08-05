ac9a0e0e81af2dedb14d0449d60c0d6d055ffd54
ryolambert <ryolambert@gmail.com>
2020-08-05 03:22:09 -0600

    FIX: zsh-syntax-highlighting path & default gh dir

--- .zshrc
+++ .zshrc
@@ -116,7 +116,8 @@ compinit
 source ~/.zsh_aliases
 
 # Set shell default directory
-mkdir ~/GitHub && cd ~/GitHub
+[ -d ~/GitHub ] && echo "" || mkdir ~/GitHub
+cd ~/GitHub
 
 # set DISPLAY variable to the IP automatically assigned to WSL2
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -17,7 +17,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
